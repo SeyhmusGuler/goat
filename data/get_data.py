cc13c0a09dd9a30e2fe178777eb9ea3e72ed6a77
Seyhmus Guler <seyhmusguler@gmail.com>
2021-07-16 14:12:31 -0400

    wrote a wrapper function to account for IEX request limits.

--- data/get_data.py
+++ data/get_data.py
@@ -1,11 +1,23 @@
-from secrets import IEX_API_TOKEN, IEX_SANDBOX_BASE_URL
+from secrets import IEX_API_TOKEN, IEX_SANDBOX_BASE_URL, WAIT_TIME_PER_REQUEST
 import requests
+import time
 
 
 def stock_endpoint_url(symbol):
     return f"{IEX_SANDBOX_BASE_URL}/stable/stock/{symbol}/quote/?token={IEX_API_TOKEN}"
 
 
+# wrapper function to account for request limits to the IEX API
+def get_data_with_request_limits(data_request_func):
+    def wrapper(*args, **kwargs):
+        data = data_request_func(*args, **kwargs)
+        time.sleep(WAIT_TIME_PER_REQUEST)
+        return data
+
+    return wrapper
+
+
+@get_data_with_request_limits
 def get_json_data_from_stock_endpoint(symbol):
     api_symbol_url = stock_endpoint_url(symbol)
     data = requests.get(api_symbol_url)
@@ -18,6 +30,7 @@ def get_json_data_from_stock_endpoint(symbol):
 
 def get_latest_price(symbol):
     data = get_json_data_from_stock_endpoint(symbol)
+    time.sleep(0.01)
     return data["latestPrice"]
 
 
