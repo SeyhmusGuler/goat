753c59e2bc217459640a26def977482a60cb2ade
Seyhmus Guler <seyhmusguler@gmail.com>
2021-07-16 14:12:59 -0400

    finalized equal weight sp500 strategy.

--- strat/equal_weight_sp500.py
+++ strat/equal_weight_sp500.py
@@ -6,7 +6,7 @@ SP500_SYMBOLS_TEXTFILE = 'data/sp500_symbols.txt'
 
 
 def get_company_list_from_file():
-    stocks = pd.read_csv(SP500_SYMBOLS_TEXTFILE, header=None)[0]
+    stocks = sorted(list(pd.read_csv(SP500_SYMBOLS_TEXTFILE, header=None)[0]))
     return stocks
 
 
@@ -14,7 +14,7 @@ def calculate_number_of_shares_to_buy(value):
     company_list = get_company_list_from_file()
     dollar_amount_per_stock = value / len(company_list)
     n_shares_to_buy = {}
-    for symbol in company_list[:10]:
+    for symbol in company_list:
         share_price = get_latest_price(symbol)
         n_shares_to_buy[symbol] = math.floor(dollar_amount_per_stock/share_price)
     return n_shares_to_buy
@@ -23,10 +23,14 @@ def calculate_number_of_shares_to_buy(value):
 class EqualWeightSP:
     def __init__(self, dollar_amount=1000):
         self.tot_value = dollar_amount
-        self.n_shares = calculate_number_of_shares_to_buy(self.tot_value)
+        self.n_shares = pd.DataFrame.from_dict(
+            calculate_number_of_shares_to_buy(self.tot_value),
+            orient='index',
+            columns=['nSharesToBuy']
+        ).reset_index().rename(columns={'index': 'Symbol'})
 
     def __str__(self):
-        return f'Equal weight sp500 with {self.tot_value} total value. Allocations: {self.n_shares}'
+        return f'Equal weight sp500 with {self.tot_value} total value.\n {self.n_shares}'
 
 
 
