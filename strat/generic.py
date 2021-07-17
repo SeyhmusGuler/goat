708f7a35e442facd21f30de85d2e283a6ba3dc85
Seyhmus Guler <seyhmusguler@gmail.com>
2021-07-17 00:06:30 -0400

    added function to chunk ticker list into 100s.

--- strat/generic.py
+++ strat/generic.py
@@ -1,6 +1,10 @@
+from utils.excel import save_to_excel_file
+
 class Strategy:
     def __init__(self, max_eq=float("inf")):
+        self.name = "OPStrategy"
         self.MAX_EQ = max_eq
+        self.position = pd.DataFrame()
 
     def __str__(self):
         return f"Max allowed equity : {self.MAX_EQ}"
@@ -10,3 +14,7 @@ class Strategy:
 
     def get_max_equity(self):
         return self.MAX_EQ
+
+    def save_to_excel(self):
+        save_to_excel_file(f"{self.name}_strategy.excel", self.position)
+
