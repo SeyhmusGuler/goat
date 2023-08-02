8cf00b1417f17bea587b66fb607cab7abda4470a
aposheker <aposheker@gmail.com>
2023-08-02 00:13:50 -0600

    added add_two method

--- goat/placeholder.py
+++ goat/placeholder.py
@@ -20,3 +20,20 @@ def multiply_by_2(x: int) -> int:
     if type(x) == int:
         return x * 2
     raise TypeError("The type of the input wasn't int.")
+    
+def add_two(x):
+    """
+    Adds 2 to the input and returns the result.
+
+    Args:
+        x (int, float): The input is number
+
+    Raises:
+        TypeError: If the input is not a number
+
+    Returns:
+        int, float: Input + 2
+    """
+    if type(x) == int or type(x) == float: 
+        return x+2
+    raise TypeError("Input is not a number.")
