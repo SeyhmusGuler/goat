41ff0b8b9b0cdd2afe55ec499bb29af3a331ac3f
Seyhmus Guler <seyhmusguler@gmail.com>
2025-12-22 20:25:41 -0700

    add settings and logger

--- src/goat/__main__.py
+++ src/goat/__main__.py
@@ -1,4 +1,12 @@
+import structlog
+from goat.settings import load_settings
+
+
 def main() -> None:
+    settings = load_settings()
+    logger = structlog.get_logger(level=settings.APP_MODE)
+    logger.info("Settings loaded", settings=settings)
+
     while True:
         pass
 
